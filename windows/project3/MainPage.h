219416ff9ac7dcbe341a4d30f28fbba0d5342ed1
Abdülkadir <abdulkadiryugruk@gmail.com>
2024-10-04 14:10:00 +0300

    New

---
+++ windows/project3/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::project3::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::project3::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
